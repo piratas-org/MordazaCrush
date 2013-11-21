ab13bde39e49f62a8fb33d6b4e979b2ef6813b45
caligari <caligari@treboada.net>
2013-11-21 14:29:14 +0100

    Permissions and app version.

---
+++ src/org/confederacionpirata/mordazacrush/MCApp.java
@@ -0,0 +1,48 @@
+package org.confederacionpirata.mordazacrush;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.pm.PackageManager.NameNotFoundException;
+
+public class MCApp extends Application {
+
+	private static Context context = null;
+	private static String appVersionName;
+	private static int appRevision;
+
+	public static Context getContext() {
+
+		return context;
+	}
+
+	@Override
+	public void onCreate() {
+
+		super.onCreate();
+
+		// app context
+		context = super.getApplicationContext();
+
+		// version
+		try {
+
+			android.content.pm.PackageInfo pi = getPackageManager()
+					.getPackageInfo(getPackageName(), 0);
+
+			appVersionName = pi.versionName;
+			appRevision = pi.versionCode;
+
+		} catch (NameNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static String appVersionName() {
+		return appVersionName;
+	}
+
+	public static int appRevision() {
+		return appRevision;
+	}
+
+}
